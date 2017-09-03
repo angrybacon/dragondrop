9c7b4fd903474db72402634729d22e97b95e9a7b
Mathieu Marques <mathieumarques78@gmail.com>
2017-09-03 15:30:30 +0200

    And then, there was light

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+class Application extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById('root'));
